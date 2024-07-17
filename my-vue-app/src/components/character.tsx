fcb4f727ecf5368e5df4c3d7f8c608f58f0fd171
Mohammad Ahmad <m.a.7snen@gmail.com>
2024-07-17 23:09:01 +0300

    adjusted character details
    Adjusted the css for CharacterDetails.

--- my-vue-app/src/components/character.tsx
+++ my-vue-app/src/components/character.tsx
@@ -2,7 +2,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "../CSS/OneCharacter.css"; // Import CSS file
+import "../CSS/CharacterDetail.css"; // Import CSS file
+import Navbar from "./Navbar";
 
 interface Character {
   id: number;
@@ -33,20 +34,25 @@ const CharacterDetail = () => {
 
   return (
     <div className="character-detail-container">
-      <img src={character.image} alt={character.name} />
-      <h1>{character.name}</h1>
-      <p>
-        <strong>Species:</strong> {character.species}
-      </p>
-      <p>
-        <strong>Status:</strong> {character.status}
-      </p>
-      <p>
-        <strong>Gender:</strong> {character.gender}
-      </p>
-      <p>
-        <strong>Origin:</strong> {character.origin.name}
-      </p>
+      <Navbar />
+      <div className="character-image">
+        <img src={character.image} alt={character.name} />
+      </div>
+      <div className="character-details">
+        <h1>{character.name}</h1>
+        <p>
+          <strong>Species:</strong> {character.species}
+        </p>
+        <p>
+          <strong>Status:</strong> {character.status}
+        </p>
+        <p>
+          <strong>Gender:</strong> {character.gender}
+        </p>
+        <p>
+          <strong>Origin:</strong> {character.origin.name}
+        </p>
+      </div>
     </div>
   );
 };
