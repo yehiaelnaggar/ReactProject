eafdae277517f3fc7cc4096d84b426f93c7b4ec0
Mohammad Ahmad <m.a.7snen@gmail.com>
2024-07-18 02:48:46 +0300

    Added pagination
    Added Pagination and removed routing from App and added it to Characters

--- my-vue-app/src/App.tsx
+++ my-vue-app/src/App.tsx
@@ -1,70 +1,10 @@
-// src/App.tsx
-import React, { useEffect, useState, } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
-
-import axios from "axios";
-import Navbar from "./components/Navbar";
-import MediaCard from "./components/card";
-import CharacterDetail from "./components/character";
-import SearchAndLogo from "./components/searchAndLogo"; // Importing the SearchAndLogo component
-import "./CSS/characters.css";
-import FavoritePage from "./components/Favourite";
-
-interface Character {
-  id: number;
-  name: string;
-  image: string;
-  species: string;
-}
+import React from "react";
+import Characters from "./components/charcters"; // Adjust the path as necessary
 
 const App = () => {
-  const [characters, setCharacters] = useState<Character[]>([]);
-
-  useEffect(() => {
-    axios
-      .get("https://rickandmortyapi.com/api/character")
-      .then((response) => {
-        setCharacters(response.data.results);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-
   return (
     <div className="app-container">
-      <BrowserRouter>
-        {/* <Navbar /> */}
-        <Routes>
-          <Route path="/" element={<SearchAndLogo />} />
-          <Route
-            path="/characters"
-            element={
-              <div className="cards-container">
-                <Navbar />
-                {characters.map((character) => (
-                  <div key={character.id} className="card-item">
-                    <MediaCard
-                      id={character.id}
-                      title={character.name}
-                      description={character.species}
-                      image={character.image}
-                    />
-                  </div>
-                ))}
-              </div>
-            }
-          />
-          <Route path="/character/:id" element={<CharacterDetail />} />
-          <Route path="/favourites" element={<><Navbar /><FavoritePage /></>} />
-          
-        </Routes>
-      </BrowserRouter>
+      <Characters />
     </div>
   );
 };
--- my-vue-app/src/components/charcters.tsx
+++ my-vue-app/src/components/charcters.tsx
@@ -1,9 +1,12 @@
-// src/App.tsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import MediaCard from "../components/card";
-import "./characters.css";
 import Navbar from "./Navbar";
+import CharacterDetail from "../components/character";
+import SearchAndLogo from "../components/searchAndLogo";
+import FavoritePage from "../components/Favourite";
+import '../CSS/characters.css';
 
 interface Character {
   id: number;
@@ -14,34 +17,56 @@ interface Character {
 
 export const Characters = () => {
   const [characters, setCharacters] = useState<Character[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const pageSize = 20; // Number of characters per page
 
   useEffect(() => {
     axios
-      .get("https://rickandmortyapi.com/api/character")
+      .get(`https://rickandmortyapi.com/api/character?page=${page}`)
       .then((response) => {
         setCharacters(response.data.results);
+        // Scroll to the top of the page when data updates
+        window.scrollTo({ top: 0, behavior: "smooth" });
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []);
+  }, [page]);
 
   return (
-    <div className="app-container">
-      <Navbar />
-      <div className="cards-container">
-        {characters.map((character) => (
-          <div key={character.id} className="card-item">
-            <MediaCard
-              id={character.id}
-              title={character.name}
-              description={character.species}
-              image={character.image}
-            />
-          </div>
-        ))}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchAndLogo />} />
+        <Route
+          path="/characters"
+          element={
+            <div className="cards-container">
+              <Navbar />
+              {characters.map((character) => (
+                <div key={character.id} className="card-item">
+                  <MediaCard
+                    id={character.id}
+                    title={character.name}
+                    description={character.species}
+                    image={character.image}
+                  />
+                </div>
+              ))}
+              <div className="pagination-container">
+                {page > 1 && ( // Render button only if page is greater than 1
+                  <button onClick={() => setPage((prev) => prev - 1)}>
+                    Prev Page
+                  </button>
+                )}
+                <button onClick={() => setPage((prev) => prev + 1)}>Next Page</button>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/character/:id" element={<CharacterDetail />} />
+        <Route path="/favourites" element={<><Navbar /><FavoritePage /></>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
